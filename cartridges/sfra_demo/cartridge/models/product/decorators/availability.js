dc288d6bfac89b372f29070744dc3166553dbc1f
Nam Luu <nam.luu@isobar.com>
2024-04-08 01:52:14 +0700

    extend product availability ATS

---
+++ cartridges/sfra_demo/cartridge/models/product/decorators/availability.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var Resource = require('dw/web/Resource');
+
+var base = module.superModule;
+
+module.exports = function (object, quantity, minOrderQuantity, availabilityModel) {
+    // Invoke the availability model on the base
+    base.call(this, object, quantity, minOrderQuantity, availabilityModel);
+
+    // Define a new property in the model with ATS as its value
+    Object.defineProperty(object, 'ats', {
+        enumerable: true,
+        value: getATSMessage(availabilityModel)
+    });
+};
+
+function getATSMessage(availabilityModel) {
+    var ATS = {};
+    ATS.messages = [];
+    var inventoryRecord = availabilityModel.inventoryRecord;
+
+    // Add a new message to the array of availability messages (just like the base does)
+    if (inventoryRecord) {
+        ATS.messages.push(
+            Resource.msgf(
+                'label.quantity.in.stock',
+                'common',
+                null,
+                inventoryRecord.ATS.value
+            )
+        );
+    }
+
+    return ATS;
+}
